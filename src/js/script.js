74fe5ac9de6dadd52058aa7605ee9c3a862c04b2
Nali <nalikante@gmail.com>
2021-04-23 08:25:34 +0200

    Add or Remove image

--- src/js/script.js
+++ src/js/script.js
@@ -79,8 +79,9 @@
 
     getElements() {
       const thisProduct = this;
-     
-      //thisProduct.imageWrapper = thisProduct.querySelector(select.menuProduct.imageWrapper);
+
+      thisProduct.imageWrapper = thisProduct.element.querySelector(select.menuProduct.imageWrapper);
+      console.log('imageWrapper', thisProduct.imageWrapper);
       thisProduct.accordionTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
       console.log('thisProduct.accordionTrigger', thisProduct.accordionTrigger);
       thisProduct.form = thisProduct.element.querySelector(select.menuProduct.form);
@@ -149,23 +150,30 @@
       for (let paramId in thisProduct.data.params) {
         // determine param value, e.g. paramId = 'toppings', param = { label: 'Toppings', type: 'checkboxes'... }
         const param = thisProduct.data.params[paramId];
-        console.log(paramId, param);
+        //console.log(paramId, param);
 
         // for every option in this category
         for (let optionId in param.options) {
           // determine option value, e.g. optionId = 'olives', option = { label: 'Olives', price: 2, default: true }
           const option = param.options[optionId];
-          console.log(optionId, option);
+          //console.log(optionId, option);
 
-        
-          if (formData[paramId]) {
-            if (formData[paramId].includes(optionId)) {
-              if (optionId.default) {
-                price = price - option.price;
-              }
-              else if (!optionId.default) {
-                price = price + option.price;
-              }
+          const optionSelected = formData[paramId] && formData[paramId].includes(optionId);
+          if (optionSelected) {
+            if (optionId.default) {
+              price = price - option.price;
+            }
+            else if (!optionId.default) {
+              price = price + option.price;
+            }
+          }
+          const optionImage = thisProduct.imageWrapper.querySelector('.' + paramId + '-' + optionId);
+          if (optionImage) {
+            if (optionSelected) {
+              optionImage.classList.add('active');
+            }
+            else {
+              optionImage.classList.remove('active');
             }
           }
         }
